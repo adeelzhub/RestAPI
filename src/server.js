6a5071040eb74c8b18300edfa103d4307771f8d9
adeelzhub <adeelijaz@hotmail.com>
2021-09-24 22:55:57 +0100

    deleted unnecessary code>server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 require("./db/connection")
 const express = require("express");
-const app = express();
-console.log(express);
-
 const routeFinder = require("./routes")
 app.use(express.json());
 app.use(routeFinder);
